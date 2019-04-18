6a4da19b44c68c9711a6b9b32431656271de46a8
user <user@IN302>
2019-04-18 18:15:54 +0900

    038

---
+++ boot-thymeleaf/src/main/java/idu/cs/controller/HomeController.java
@@ -0,0 +1,37 @@
+package idu.cs.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import idu.cs.domain.User;
+import idu.cs.repository.UserRepository;
+
+@Controller
+public class HomeController {
+@Autowired UserRepository userRepo;
+
+	@GetMapping("/")
+	public String home(Model model) {
+		model.addAttribute("test", "인덕컴소");
+		model.addAttribute("ssh", "서승현");
+		return "index";
+	}
+	@GetMapping("/users")
+	public String getAllUser(Model model) {
+		model.addAttribute("users", userRepo.findAll());
+		return "userlist";
+	}
+	@PostMapping("/users")
+	public String createUser(@Valid @RequestBody User user, Model model) {
+		userRepo.save(user);
+		model.addAttribute("user", userRepo.findAll());
+		return "redirect:/users";
+	}
+
+}
